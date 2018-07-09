e20c4c732326b15bab21b5d2a5726104681bc9bf
Matthias Molitor <matthias@molitor.solutions>
2018-07-09 15:29:46 +0200

    implemented product update test

--- spring-kotlin/src/test/kotlin/spring/product/ProductPortTest.kt
+++ spring-kotlin/src/test/kotlin/spring/product/ProductPortTest.kt
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
 import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpEntity
+import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
 import org.springframework.test.context.junit4.SpringRunner
 import spring.App
@@ -78,7 +80,16 @@ class ProductPortTest {
 
     @Test
     fun `can update existing product`() {
+        anExistingProduct("42")
 
+        val updatedProduct = ProductView("42", "Wein", "12345")
+        testRestTemplate.exchange("/products/42", HttpMethod.PUT, HttpEntity(updatedProduct), String::class.java)
+
+        val productFromApi = getProduct("42")
+        Assertions.assertThat(productFromApi).isNotNull
+        Assertions.assertThat(productFromApi!!.name)
+                .isNotNull()
+                .isEqualTo("Wein")
     }
 
     @Test
